1b949f437a6f7b844110f0ef4e7dfc7aaa1030f2
Adinath more <moreadi7777@gmail.com>
2025-07-09 19:22:32 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
+function countVowels(str){
+    let count = 0;
+    for (const char of str){
+        if (char === "a" || 
+            char === "e" || 
+            char === "i" || 
+            char === "o" || 
+            char === "u") 
+        {
+            count++;
+        }
+    }
+    return count;
+ }
 
-let score = prompt("enter your score(0-100):");//yachyamadhe gelyavr 1 te 100
-//  madhil word takaych output (a,b,c,d,f) as yenar
-let grade;  
-
-if (score >= 90 && score <=100){
-    grade = "A";
-}
-else if (score >=70&& score <=89){
-    grade = "B";
-}
-else if (score >=60&& score <=69){
-    grade = "C";
-}
-else if (score >=50&& score <=59) {
-    grade = "D";
-}
-else if (score >=0&& score <=49){
-    grade = "F";
-}
-
-console.log("according to your scores, your grade  : " ,grade);
+ //this is a vowels
