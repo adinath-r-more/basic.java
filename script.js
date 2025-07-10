0b751aee595b80a0d3f5cffae819f5b321202f6d
Adinath more <moreadi7777@gmail.com>
2025-07-10 18:55:53 +0530

    intial commit

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-function countVowels(str){
-    let count = 0;
-    for (const char of str){
-        if (char === "a" || 
-            char === "e" || 
-            char === "i" || 
-            char === "o" || 
-            char === "u") 
-        {
-            count++;
-        }
-    }
-    return count;
- }
+// function countVowels(str){
+//     let count = 0;
+//     for (const char of str){
+//         if (char === "a" || 
+//             char === "e" || 
+//             char === "i" || 
+//             char === "o" || 
+//             char === "u") 
+//         {
+//             count++;
+//         }
+//     }
+//     return count;
+//  }
 
- //this is a vowel
+//  //this is a vowel`
+
+let arr = [1, 2, 3, 4, 5];
+
+arr.forEach(function printVal(val){
+
+})
