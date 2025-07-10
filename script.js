ef40607ef1df00a7197c9783f7548be9f3e80a38
Adinath more <moreadi7777@gmail.com>
2025-07-10 19:28:55 +0530

    Initial commit

--- script.js
+++ script.js
@@ -15,8 +15,14 @@
 
 //  //this is a vowel`
 
-let arr = ["mumbai", "pune", "talmod", "hyd"];
+// let arr = ["mumbai", "pune", "talmod", "hyd"];
 
-arr.forEach ((val) => {
-    console.log(val.toUpperCase());
-})
+// arr.forEach ((val) => {
+//     console.log(val.toUpperCase());
+// })
+
+let nums = [2, 3, 4, 5, 6, 7];
+
+nums.forEach((num) => {
+    console.log(num*num); //num**2
+});
